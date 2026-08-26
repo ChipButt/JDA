33e881005c9d40a2886253ac33c7b2f523e0b9bb
ChipButt <jameschipbutt@hotmail.com>
2026-08-26 08:43:38 +0100

    Cache custom icons and photo placeholder update

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE = 'jda-static-v6';
-const ASSETS = ['./', './index.html', './styles.css', './icons.css', './app-core.js', './app-walking.js', './app-sitting.js', './app-route.js', './app-ui.js', './manifest.webmanifest', './icon.svg', './assets/icons/dogs.svg', './assets/icons/sitting.svg', './assets/icons/walks.svg', './assets/icons/home.svg', './assets/icons/route.svg'];
+const CACHE = 'jda-static-v7';
+const ASSETS = ['./', './index.html', './styles.css', './icons.css', './photo.css', './app-core.js', './app-walking.js', './app-sitting.js', './app-route.js', './app-ui.js', './app-photo.js', './manifest.webmanifest', './icon.svg', './assets/icons/dogs.svg', './assets/icons/sitting.svg', './assets/icons/walks.svg', './assets/icons/home.svg', './assets/icons/route.svg', './assets/dog-placeholder.svg'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
 self.addEventListener('fetch', event => {
