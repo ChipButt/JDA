9f2f9ab8960327303a3c761938be3dab631383c9
ChipButt <jameschipbutt@hotmail.com>
2026-08-26 08:12:00 +0100

    Cache v4 app modules

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 const CACHE = 'jda-static-v4';
-const ASSETS = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest', './icon.svg'];
+const ASSETS = ['./', './index.html', './styles.css', './app-core.js', './app-sitting.js', './app-route.js', './app-ui.js', './manifest.webmanifest', './icon.svg'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
 self.addEventListener('fetch', event => {
